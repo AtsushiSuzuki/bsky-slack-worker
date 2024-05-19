4f5a7cb0dae5786634244dec7cd6c47ff19ca96c
Atsushi Suzuki <possibly.maybe.probably@gmail.com>
2024-05-20 00:03:51 +0900

    更新

--- src/index.ts
+++ src/index.ts
@@ -87,34 +87,69 @@ export default {
 					continue;
 				}
 
+				const blocks = [];
+				const contextElements: any[] = [];
+				if (post.author.avatar) {
+					contextElements.push({
+						type: "image",
+						image_url: post.author.avatar,
+						alt_text: `${post.author.displayName ?? post.author.handle}'s avatar`,
+					});
+				}
+				contextElements.push({
+					type: "plain_text",
+					text: post.author.displayName ?? post.author.handle,
+				});
+				blocks.push({
+					type: "context",
+					elements: contextElements,
+				})
+				if (record.text) {
+					blocks.push({
+						type: "section",
+						text: {
+							type: "plain_text",
+							text: record.text,
+							emoji: true,
+						}
+					});
+				}
+				if (post.embed && post.embed.$type === "app.bsky.images#view") {
+					const images = post.embed.images as any[];
+					for (const image of images) {
+						blocks.push({
+							type: "image",
+							image_url: image.thumb,
+							alt_text: image.alt || "",
+						});
+					}
+				}
+				blocks.push({
+					type: "actions",
+					elements: [
+						{
+							type: "button",
+							text: {
+								type: "plain_text",
+								text: "Open in bsky.app",
+							},
+							value: post.uri,
+							url: `https://bsky.app/profile/${identifier}/post/${postId}`,
+							action_id: "button-action",
+						},
+					],
+				});
+
+				console.log("post: ", post);
+				console.log("blocks: ", blocks);
+
 				await fetch(env.SLACK_WEBHOOK_URL, {
 					method: "post",
 					headers: {"Content-Type": "application/json"},
-					body: JSON.stringify({
-						blocks: [
-							{
-								type: "section",
-								text: {
-									type: "plain_text",
-									text: record.text,
-									emoji: true,
-								},
-								accessory: {
-									type: "button",
-									text: {
-										type: "plain_text",
-										text: "Open in bsky.app",
-										emoji: true,
-									},
-									value: post.uri,
-									url: `https://bsky.app/profile/${identifier}/post/${postId}`,
-									action_id: "button-action",
-								},
-							},
-						],
-					}),
-				}).then(res => {
+					body: JSON.stringify({blocks}),
+				}).then(async res => {
 					if (!res.ok) {
+						console.log(`submit to slack failed: ${res.status}: ${res.statusText}\n${await res.text()}`);
 						throw new Error(`submit to slack failed: ${res.status}: ${res.statusText}`);
 					}
 					console.log(`post submitted: ${post.uri}`);
